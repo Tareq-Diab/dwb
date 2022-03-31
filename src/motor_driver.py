8554dcda9366390dd4c6db07c7d859e32a64c4b0
Tariq96 <tareq.a.diab@gmail.com>
2022-03-31 04:09:41 +0200

    tuned pid and fixed some errors

--- src/motor_driver.py
+++ src/motor_driver.py
@@ -23,8 +23,8 @@ pwm_ml.start(0)
 v=0.5
 z=0.5
 
-pwm_mr.ChangeDutyCycle(speed)
-pwm_ml.ChangeDutyCycle(speed)
+pwm_mr.ChangeDutyCycle(0)
+pwm_ml.ChangeDutyCycle(0)
 
 rospy.init_node("key_listenser_node")
 
@@ -34,8 +34,10 @@ class motors:
         self.error_r_d=0
         self.left_speed=motorspeed(16,231)
         self.right_speed=motorspeed(16,231)
-        self.kp=2
-        self.kd=2
+        self.error_l_tprev=0
+        self.error_r_tprev=0
+        self.kp=0.3
+        self.kd=0
         self.ki=0
     def set_rpm(self,l,r):
         if l>0:
@@ -52,20 +54,23 @@ class motors:
             gpio.output(mr[0],1)
         error_l =l-self.left_speed.RPM()
         error_l_d=error_l-self.error_l_tprev
-        pwm_l=self.kp*error_l+self.kd*self.error_l_d 
-        pwm_l =pwm_l if pwm_l<100 else 100
-        pwm_ml.ChangeDutyCycle(pwm_l)
-        error_l_tprev=error_l
+        pwm_l=self.kp*error_l+self.kd*self.error_l_d
+        print(pwm_l)
+        pwm_l =abs(pwm_l) if abs(pwm_l)<100 else 100
+        pwm_ml.ChangeDutyCycle(abs(pwm_l))
+        self.error_l_tprev=error_l
+        
         
         error_r =r-self.right_speed.RPM()
         error_r_d=error_r-self.error_r_tprev
         pwm_r=self.kp*error_r+self.kd*self.error_r_d 
-        pwm_r =pwm_r if pwm_r<100 else 100
-        pwm_mr.ChangeDutyCycle(pwm_r)
-        error_r_tprev=error_r   
+        pwm_r =abs(pwm_r) if abs(pwm_r)<100 else 100
+        pwm_mr.ChangeDutyCycle(abs(pwm_r))
+        self.error_r_tprev=error_r   
         
 wheel_span=0.275
 wheel_circumference=0.082*3.14
+motors=motors()
 def twis_CB(msg):
     v=msg.linear.x
     w=msg.angular.z
@@ -74,4 +79,6 @@ def twis_CB(msg):
     w_r=int((v_r/wheel_circumference)*60)
     w_l=int((v_l/wheel_circumference)*60)
     motors.set_rpm(w_l,-w_r)
-rospy.rospy.Subscriber("cmd_vel", Twist, twis_CB)
+rospy.Subscriber("cmd_vel", Twist, twis_CB)
+while True:
+    rospy.spin()
